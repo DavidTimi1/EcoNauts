5757a287c5bdfef1b87f2a28dc7b209399b3898e
David <duwagbale07@gmail.com>
2025-01-23 14:12:40 +0100

    Update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,9 +7,9 @@ export default function Home(){
 
     return (
         <section id="home" className="m-0 w-full bg-gradient-to-b gradient scroll-pt-10">
-            <div className="absolute w-full h-full top-0 left-0 opacity-50" style={{backgroundImage: `url(${mainBg})`}}>
+            <div className="bg-overlay absolute w-full h-full top-0 left-0 opacity-70 bg-no-repeat bg-center bg-cover" style={{backgroundImage: `url(${mainBg})`}}>
             </div>
-            <div className="w-full py-10">
+            <div className="w-full py-10" style={{minHeight: "80vh"}}>
                 <div className="w-full p-2 md:px-5 flex items-center justify-center">
                     <div className="flex flex-col gap-2 text-center">
 
