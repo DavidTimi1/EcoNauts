30f5d9a9d24845432781fab7e89d9bad9a32991d
David <duwagbale07@gmail.com>
2025-01-23 10:43:51 +0100

    Feat (about)

---
+++ src/components/about.jsx
@@ -0,0 +1,26 @@
+import { BrandedName } from "../ui/logo";
+
+
+export default function About(){
+
+    return (
+
+        <section id="about" className="py-10 w-full bg-green-light">
+            <div className="w-full p-2 md:px-5 flex flex-col items-center justify-center gap-3">
+                <h2 className="text-4xl">
+                    About <span> Us </span>
+                </h2>
+
+
+				<p style={{maxWidth: "min(80%, 500px)"}}>
+                    At 
+                    <span style={{textShadow: "0 0 3px black"}} className="text-xl">
+                        <BrandedName />
+                    </span>, we combine entertainment and education to promote sustainability and forest conservation.
+				   Join us in our journey to create a greener future.
+                </p>
+
+            </div>
+        </section>
+    )
+}
---
+++ src/components/innovations.jsx
@@ -0,0 +1,23 @@
+
+
+export default function Innovations(){
+
+    return (
+        <section id="innovations" className="my-10 w-full bg-green-lime">
+            <div className="w-full p-2 md:px-5 flex flex-col items-center justify-center">
+                <h2>
+                    Innovations
+                </h2>
+                
+                <p><small>
+                    
+                </small></p>
+
+
+
+
+
+            </div>
+        </section>
+    )
+}
