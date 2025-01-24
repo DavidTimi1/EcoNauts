2bc6e50a49b8df2c1fbf3faecad996037418b450
David <duwagbale07@gmail.com>
2025-01-24 23:56:48 +0100

    Fix (contact)

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,7 +8,7 @@ const ContactUs = () => {
 						Get in <span> Touch </span>
 					</h2>
 
-					<p className="text-sm text-gray-300 max-w-3xl">
+					<p className="text-sm text-gray-300 max-w-xl">
 						We’d love to hear from you! Whether you have a question or want to book an appointment, we’re here to help.
 					</p>
 				</div>
@@ -17,23 +17,23 @@ const ContactUs = () => {
 				<div className="w-full mx-auto">
 					{/* Form */}
 					<div className="bg-white text-black rounded-lg shadow-lg p-3 md:p-6">
-						<form className="space-y-6">
-							<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+						<form className="space-y-3">
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-2">
 								{/* Name */}
-								<div>
-									<label htmlFor="name" className="block text-sm font-medium">
+								<div className="flex flex-col gap-1">
+									<label htmlFor="name" className="text-sm font-medium">
 										Your Name
 									</label>
 									<input
 										type="text"
 										id="name"
 										placeholder="Enter your name"
-										className="w-full mt-2 p-3 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500"
+										className="w-full p-1 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500"
 									/>
 								</div>
 
 								{/* Email */}
-								<div>
+								<div className="flex flex-col gap-1">
 									<label htmlFor="email" className="block text-sm font-medium">
 										Your Email
 									</label>
@@ -41,13 +41,13 @@ const ContactUs = () => {
 										type="email"
 										id="email"
 										placeholder="Enter your email"
-										className="w-full mt-2 p-3 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500"
+										className="w-full p-1 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500"
 									/>
 								</div>
 							</div>
 
 							{/* Message */}
-							<div>
+							<div className="flex flex-col gap-1">
 								<label htmlFor="message" className="block text-sm font-medium">
 									Message
 								</label>
@@ -55,7 +55,7 @@ const ContactUs = () => {
 									id="message"
 									rows="4"
 									placeholder="Write your message"
-									className="w-full mt-2 p-3 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500"
+									className="w-full p-1 border border-gray-300 rounded-lg focus:ring-green-500 focus:border-green-500"
 								></textarea>
 							</div>
 
@@ -63,7 +63,7 @@ const ContactUs = () => {
 							<div className="text-center">
 								<button
 									type="submit"
-									className="w-full md:w-auto bg-green-500 hover:bg-green-600 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transition duration-300"
+									className="w-full md:w-auto bg-green-dark hover:bg-green-600 text-white font-semibold px-2 py-1 rounded-lg shadow-lg transition duration-300"
 								>
 									Send Message
 								</button>
